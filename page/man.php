68b0cff617b320ba2f49dc7633aa6d3f5398e1c5
segasle <segasle@gmail.com>
2019-10-17 15:34:31 +0300

    head

--- page/man.php
+++ page/man.php
@@ -1,7 +1,25 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: sergejslepenkov
- * Date: 2019-09-05
- * Time: 13:20
- */
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta property="og:locale" content="ru_RU">
+    <meta property="og:type" content="article">
+    <meta property="og:title" content="Веб студия - webpro">
+    <meta property="og:description" content="ru_RU">
+    <meta property="og:site_name"
+          content="">
+    <meta name="robots" content="index, follow">
+    <meta name="keywords"
+          content="">
+    <meta name="description" content="">
+    <title></title>
+    <link rel="icon" href="/images/logo-light.png" type="image/png" sizes="<16>X<16>">
+    <link rel="stylesheet" href="/icons/css/all.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/style.css?t=<?php echo(microtime(true) . rand()); ?>">
+</head>
+</html>
