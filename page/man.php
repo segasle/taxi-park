f003454e43e79b57977a90dfecd7fa1455903bc1
sergejslepenkov <Segasle@gmail.com>
2019-10-17 16:14:20 +0300

    wrap

--- page/man.php
+++ page/man.php
@@ -22,4 +22,11 @@
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/style.css?t=<?php echo(microtime(true) . rand()); ?>">
 </head>
+<body>
+<div class="wrapper">
+    <header></header>
+    <main></main>
+    <footer></footer>
+</div>
+</body>
 </html>
