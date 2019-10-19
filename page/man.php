959819b2363ad681ccd1806315358a2ef4ed709f
sergejslepenkov <Segasle@gmail.com>
2019-10-19 17:42:37 +0300

    js

--- page/man.php
+++ page/man.php
@@ -102,9 +102,9 @@
                     <?php
                     $sql = mysqli('SELECT * FROM `work`');
                     foreach ($sql as $item) {
-                        if ($item['stock'] != null){
-                            $stock = ' - <i class="text-danger">'.$item['stock'].'</i>';
-                        }else{
+                        if ($item['stock'] != null) {
+                            $stock = ' - <i class="text-danger">' . $item['stock'] . '</i>';
+                        } else {
                             $stock = '';
                         }
                         ?>
@@ -137,4 +137,13 @@
     </footer>
 </div>
 </body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
 </html>
