ab03dab0293d9f94012c4bad267a422b64eeef97
segasle <segasle@gmail.com>
2019-10-19 08:45:21 +0300

    бд

--- functions/bd.php
+++ functions/bd.php
@@ -5,3 +5,16 @@
  * Date: 2019-09-05
  * Time: 13:00
  */
+
+global $mysqli;
+$bd = 'ca57629_taxipark';
+$password = 'qLX1xXnw';
+if (empty($mysqli)){
+    $mysqli = mysqli_connect('localhost', $bd, $password, $bd);
+    mysqli_set_charset($mysqli, 'UTF8');
+}
+if (mysqli_connect_errno()){
+    echo 'ошибка в подключении к БД ('.mysqli_connect_errno().')'.mysqli_connect_error();
+}
+
+
